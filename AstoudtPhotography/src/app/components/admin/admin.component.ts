5839dc56a9bc94dadcc0c2fb83e08070abe974be
Brett Stoudt <bstoudt01@gmail.com>
2025-02-13 12:50:40 -0800

    reassert files hoping that the directory structure is fixed in git

---
+++ AstoudtPhotography/src/app/components/admin/admin.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ImageService } from '../../services/image/image.service'
+import { FileUploadComponent } from '../file-upload/file-upload.component';
+@Component({
+  selector: 'app-admin',
+  imports: [FileUploadComponent],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+
+//Create a internal class to use this imageService from constructior?
+// class uploadHandler {
+//   constructor(private imageService: ImageService) {}
+//   getCars(): string[] {
+//     return this.cars;
+//   }}
+export class AdminComponent {
+//   imageService: ImageService
+// const abc= @new uploadHandler(this.imageService);
+//   selectedFile: File | null = null;
+//   imageService = 
+//   onFileSelected(event: Event): void {
+//     const input = event.target as HTMLInputElement;
+//     if (input.files && input.files.length > 0) {
+//       this.selectedFile = input.files[0];
+//     }
+//   }
+  
+}
